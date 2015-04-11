b98b6711823c02a18e9a059874e34f89e5e03b26
murrayds <dakota.s.murray@gmail.com>
2015-04-11 11:52:48 -0400

    Update

--- Bitcamp/DetailController.swift
+++ Bitcamp/DetailController.swift
@@ -6,32 +6,70 @@
 //  Copyright (c) 2015 Dakota Murray. All rights reserved.
 //
 
+import TwitterKit
+
 class DetailController: UIViewController {
     
     private let TAG = "DetailController"
     
     private let START_LAT = 38.9875
-    private let START_LONG = 76.9400
+    private let START_LONG = -76.9400
+    
+    private var loginViewController = UIViewController()
+    
+    private var mapView: GMSMapView?
+    
+    
+    
+    /**
+    
+    */
+    override func viewDidLoad() {
+        Logger.log(TAG, message: "\(__FUNCTION__)")
+        super.viewDidLoad()
+        
+        
+        var backButton = UIBarButtonItem(
+            title: "Events",
+            style: UIBarButtonItemStyle.Bordered,
+            target: self,
+            action: "openEvents:")
+        
+        
+        navigationItem.leftBarButtonItem = backButton
+        
+        var camera = GMSCameraPosition.cameraWithLatitude(START_LAT,
+            longitude: START_LONG, zoom: 15)
+        
+        mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+
+        mapView?.myLocationEnabled = true
+        self.view = mapView
+        
+        drawCircle(START_LAT, long: START_LONG, radius: 100)
+    }
     
     
     /**
 
     */
-    override func viewDidLoad() {
+    func openEvents(sender: UIBarButtonItem) {
         Logger.log(TAG, message: "\(__FUNCTION__)")
-        
-        var camera = GMSCameraPosition.cameraWithLatitude(START_LAT,
-            longitude: START_LONG, zoom: 6)
-        
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        
-        mapView.myLocationEnabled = true
-        self.view = mapView
-        
-        var marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "University of Maryland, College Park"
-        marker.snippet = "Maryland"
-        marker.map = mapView
+        splitViewController?.preferredDisplayMode = UISplitViewControllerDisplayMode.AllVisible
     }
+    
+    /**
+    
+    */
+    private func drawCircle(lat: Double, long: Double, radius: Double) {
+        let circleCenter = CLLocationCoordinate2DMake(lat, long);
+        let circ = GMSCircle(position:circleCenter, radius:radius)
+        
+        circ.fillColor = UIColor(red:225, green:0, blue:0, alpha:0.75)
+        circ.strokeColor = UIColor.redColor()
+        circ.strokeWidth = 5
+        circ.map = self.mapView
+    }
+    
+    
 }
---
+++ Bitcamp/MapModel.swift
@@ -0,0 +1,16 @@
+//
+//  MapModel.swift
+//  Bitcamp
+//
+//  Created by Dakota Murray on 4/11/15.
+//  Copyright (c) 2015 Dakota Murray. All rights reserved.
+//
+
+class MapModel: NSObject, CLLocationManagerDelegate {
+    
+    private let TAG = "MapModel"
+    
+    
+    
+    
+}
